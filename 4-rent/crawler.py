4f51768edf4fa0fb65ffb810f565a3e439e671bf
Huu <742790905@qq.com>
2017-08-07 20:22:14 +0800

    rent修改

--- 4-rent/crawler.py
+++ 4-rent/crawler.py
@@ -13,18 +13,15 @@ while True:
     print('featch :',url.format(page=page))
     d=pq(url=url.format(page=page))
     house_list=d('.list>li')
-    
     if not house_list:
         break
 
-    for house in house_list:
-        house=pq(house)
+    for house in house_list.items():
         house_title=house('.des h2').text()
         house_url=urljoin(url,house('a').attr('href'))
         house_info_list=house_title.split()
         house_loc=house_info_list[1]
-        house_money=house('.money span b').text()
-        
+        house_money=house('.money span b').text()     
         csv_writer.writerow([house_title,house_loc,house_url,house_money])
-
+        
 csv_file.close()
